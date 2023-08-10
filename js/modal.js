25ebf2c3f1dd8f5aea59d9a344b9226347159c93
Yvhenii <jekalyapa@gmail.com>
2023-08-10 14:37:19 +0200

    model

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+window.addEventListener("DOMContentLoaded", () => {
+  const openModalBtn = document.querySelector(".open_btn");
+  const closeModalBtn = document.querySelector(".modal-close");
+  const modal = document.querySelector(".overlay");
+
+  openModalBtn.addEventListener("click", () => {
+    modal.classList.add("show");
+  });
+  closeModalBtn.addEventListener("click", () => {
+    modal.classList.remove("show");
+  });
+});
